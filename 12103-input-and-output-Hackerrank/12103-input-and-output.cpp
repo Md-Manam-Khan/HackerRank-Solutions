540dde708df9ed7df5eb5b4398105b3eb5854414
Md. Manam Khan <manamkhan921@gmail.com>
2026-08-15 21:46:42 +0600

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 12103-input-and-output-Hackerrank/12103-input-and-output.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll a, b, c;
+    cin>>a>>b>>c;
+    cout<<a + b + c;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
